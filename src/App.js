4ca6924535d4a0141c8b6985e7848d152ea59325
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-24 12:33:01 -0700

    first post template

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav';
+import Readits from './Readits';
 import { BrowserRouter as Router} from "react-router-dom";
 
+//paages to create: 
+// - profile
+// - notifications
+
+// - best
+// - hot 
+// - new
 
 function App() {
 
   //useState and useEffect here 
-
+  //const [feed, setFeed]? 
   //JS functions 
+
+  //upvote/downvote functions
 
   return (
     <Router>
     <div className="App">
       <Nav 
+      />
+      <Readits
       />
 
     </div>
---
+++ src/Readits.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+function Readits() {
+
+    return (
+        <div className="readit-holder">
+            <div className="readit-posts">
+                <div className="posted-by">Posted by u/Funee45</div>
+                <div className="title">Your perception is true. We've had 10x rainfall this year over last.</div>
+                <div className="post-content">
+                    <img className="post-image" src="https://bloximages.newyork1.vip.townnews.com/azfamily.com/content/tncms/assets/v3/editorial/c/96/c96ba39e-015d-11ec-ae2c-c35b8d9a2917/611f12c01b2c1.image.png?resize=750%2C422" alt="valley rainfall since 1990"></img>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Readits;
