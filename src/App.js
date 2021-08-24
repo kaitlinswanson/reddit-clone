6539d4a336a9ed727cc2f28ad9e81146f1afe450
Kaitlin Swanson <kateswanson94@gmail.com>
2021-08-24 10:35:51 -0700

    header/nav added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import { BrowserRouter as Router} from "react-router-dom";
+
 
 function App() {
+
+  //useState and useEffect here 
+
+  //JS functions 
+
   return (
+    <Router>
     <div className="App">
+      <Nav 
+      />
 
     </div>
+
+
+    </Router>
+
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import {Link} from 'react-router-dom';
+
+function Nav() { 
+
+//JS here
+
+    return (
+        <nav className="Nav">
+            <Link id="home-link" to='/'>
+            <h2><i id="face" className="fas fa-grin"></i> readit</h2>
+            </Link>
+            <input id="searchbar" type="text" placeholder="Search..."></input>
+            <ul className="nav-links">
+                <Link id="notifications" to='/notifications'>
+                    <li><i className="far fa-bell"></i></li>
+                </Link>
+                <Link id="profile" to='/profile'>
+                    <li><i className="far fa-user"></i></li>
+                </Link>
+            </ul>
+        </nav>
+      );
+    }
+    
+    export default Nav;
+
+
+
